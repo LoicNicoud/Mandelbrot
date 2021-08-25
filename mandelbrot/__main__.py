3d98ddae867680027633a048fbc5d9cacc1bedbb
LoicNicoud <38888940+LoicNicoud@users.noreply.github.com>
2021-08-25 16:38:14 +0200

    first impl pyopencl - not cleaned

--- mandelbrot/__main__.py
+++ mandelbrot/__main__.py
@@ -1,42 +1,51 @@
-from .mandelbrot import Mandelbrot
+from time import time
 import click
+from .mandelbrot import Mandelbrot
 
 
 @click.command(help='Compute and colorize the mandelbrot set around a certain point')
 @click.option('-w', '--width',
-              default=1500, type=int, show_default=True,
+              default=6000, type=int, show_default=True,
               help='Width of the output image')
 @click.option('-h', '--height',
-              default=1000, type=int, show_default=True,
+              default=3200, type=int, show_default=True,
               help='Height of the output image')
 @click.option('-rc', '--real-center',
-              default=-0.7106, type=float, show_default=True,
+              default=-0.743643887037151, type=float, show_default=True,
               help='Real part of the center point')
 @click.option('-ic', '--imag-center',
-              default=+0.246575, type=float, show_default=True,
+              default=0.13182590420533, type=float, show_default=True,
               help='Imaginary part of the center point')
 @click.option('-rw', '--real-width',
-              default=0.0084, type=float, show_default=True,
+              default=0.0015, type=float, show_default=True,
               help='wiew width of the set')
 @click.option('-m', '--max-iter',
-              default=1500, type=int, show_default=True,
+              default=500, type=int, show_default=True,
               help='Maximum iterations per point')
 @click.option('-s', '--seed',
               default=0, type=int, show_default=True,
               help='Seed for the colorgradient, -1 means the default colormap, 0 a random seed, or the seed number')
 @click.option('-o', '--out',
-              default='images/mandel.jpg', type=click.Path(), show_default=True,
+              default='images/mandel.png', type=click.Path(), show_default=True,
               help='output image path')
 def main(width, height, real_center, imag_center, real_width, max_iter, out, seed):
     mandel = Mandelbrot(width, height, real_center,
                         imag_center, real_width, max_iter)
 
+    start_time = time()
     click.echo('Computing set')
-    mandel.compute_set()
+    mandel.compute_set(seed=seed)
+    click.echo(f'Done in :{time() - start_time:.4f}s.')
 
-    seed = mandel.create_colormap(seed)
-    click.echo(f'Colorizing set with seed: {seed}')
-    mandel.draw()
+    # start_time = time()
+    # click.echo('Creating color map')
+    # seed = mandel.create_colormap(seed)
+    # click.echo(f'Done in :{time() - start_time:.4f}s.')
+
+    # start_time = time()
+    # click.echo(f'Colorizing set with seed: {seed}')
+    # mandel.draw()
+    # click.echo(f'Done in :{time() - start_time:.4f}s.')
 
     mandel.save(out)
     click.echo(f'Image saved to: {out}')
--- mandelbrot/mandelbrot.py
+++ mandelbrot/mandelbrot.py
@@ -1,85 +1,116 @@
 import numpy as np
 import numba
 from PIL import Image
-from math import log, floor, ceil
+from math import log, floor, ceil, sin
 from scipy.interpolate import interp1d
 import random
+import pyopencl as cl
+import pyopencl.array as cl_array
+import time
 
 N_COLORS = 2000
 
 
-@numba.jit
-def mandel(c, max_iter):
-    z = c
-    for n in range(max_iter):
-        if z.real * z.real + z.imag * z.imag > 4.0:
-            return n + 1 - log(log(abs(z)))/log(2)
-        z = z*z + c
-    return max_iter
+def comp_mandelset_opencl(cgrid: np.ndarray, maxiter: int, ampl, ofst, per):
+    """Compute the mandelbrot set iteration with pyopencl."""
+    ctx = cl.create_some_context()
+    queue = cl.CommandQueue(ctx)
+
+    # out = np.empty(cgrid.shape, dtype=np.double)
+    out = np.empty(cgrid.shape, dtype=np.uint32)
+
+    mf = cl.mem_flags
+    q_opencl = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=cgrid)
+    output_opencl = cl.Buffer(ctx, mf.WRITE_ONLY, out.nbytes)
+
+    # ampl_buff = cl.Buffer(ctx, mf.READ_ONLY, ampl.nbytes)
+    # ofst_buff = cl.Buffer(ctx, mf.READ_ONLY, ofst.nbytes)
+    # per_buff = cl.Buffer(ctx, mf.READ_ONLY, per.nbytes)
+    ampl_dev = cl_array.to_device(queue, ampl)
+    ofst_dev = cl_array.to_device(queue, ofst)
+    per_dev = cl_array.to_device(queue, per)
+
+    prg = cl.Program(
+        ctx,
+        """
+
+    __kernel void mandelbrot(__global double2 *q,
+                     __global uchar4 *output, ushort const maxiter, 
+                     __global double *ampl, __global double *ofst, __global double *per)
+    {
+        int gid = get_global_id(0);
+        double x = 0.0f;
+        double y = 0.0f;
+        double x2 = 0.0f;
+        double y2 = 0.0f;
+        double w = 0.0f;
+        uint curiter = 0;
+        double normiter = 0.0f;
+
+        output[gid] = 0.0f;
+
+        while ((x2 + y2 <= 4.0f) && (curiter < maxiter)) {
+            y = (x + x) * y + q[gid].y;
+            x = x2 - y2 + q[gid].x;
+            x2 = x * x;
+            y2 = y * y;
+            curiter++;
+        }
+
+        if (curiter < maxiter) {
+            normiter = (float) curiter;
+            normiter += 1 - log(log2(sqrt(x2 + y2)));
+            output[gid].x = (int) (sin(per[0] * normiter) * ampl[0] + ofst[0]);
+            output[gid].y = (int) (sin(per[1] * normiter) * ampl[1] + ofst[1]);
+            output[gid].z = (int) (sin(per[2] * normiter) * ampl[2] + ofst[2]);
+            output[gid].w = 255;
+        }
+        else {
+            output[gid].x = 0;
+            output[gid].y = 0;
+            output[gid].z = 0;
+            output[gid].w = 0;
+        }
+
+        
+
+    }
+    """,
+    ).build()
+
+    prg.mandelbrot(
+        queue, out.shape, None, q_opencl, output_opencl, np.uint16(
+            maxiter), ampl_dev.data, ofst_dev.data, per_dev.data
+    )
+
+    cl.enqueue_copy(queue, out, output_opencl).wait()
+    # byt = out.tobytes()
+    # out = np.frombuffer(byt, dtype=np.uint8)
+
+    return out
 
 
-@ numba.jit
-def mandel_set(re_min, re_max, im_min, im_max, width, height, max_iter):
-    c_map = np.zeros((height, width), dtype=np.cdouble)
-    i_map = np.zeros((height, width))
-    for y in range(height):
-        for x in range(width):
-            re = (x / (width-1)) * (re_max - re_min) + re_min
-            im = (1 - y / (height-1)) * (im_max - im_min) + im_min
-            c_map[y][x] = complex(re, im)
+def colorize(iter_map, max_iter, v_freq, v_phase):
+    hi, wi = iter_map.shape
+    img = np.zeros((hi, wi, 3), dtype=np.uint8)
 
-    for i, c in enumerate(c_map.flat):
-        i_map.flat[i] = mandel(c, max_iter)
-
-    return i_map
-
-
-def colormap(controlpoints):
-    i_c = [0.0, .6, .85, 1.0]
-
-    xi = np.linspace(0, 1, N_COLORS)
-    fc = interp1d(i_c, controlpoints)
-
-    col_map = np.zeros((N_COLORS, 3))
-    for i, x in enumerate(xi):
-        col_map[i, :] = fc(x)
-
-    return col_map
-
-
-@ numba.jit
-def colorize(i_map, max_iter, c_map):
-    x, y = i_map.shape
-    img = np.zeros((x, y, 3), dtype=np.uint8)
-
-    hist = np.zeros(max_iter)
-    for it in i_map.flat:
-        if it < max_iter:
-            hist[floor(it)] += 1
-    total = hist.sum()
-
-    c_range = np.zeros((max_iter + 1))
-    h = 0
-    for i in range(max_iter):
-        h += hist[i] / total
-        c_range[i] = h
-    c_range[-1] = 1.0
-
-    for i, row in enumerate(i_map):
-        for j, it in enumerate(row):
+    for x in range(wi):
+        for y in range(hi):
+            it = iter_map[y, x]
             if it >= max_iter:
-                img[i, j, :] = [0, 0, 0]
+                img[y, x] = [0, 0, 0]
             else:
-                idx = int(N_COLORS * lin_inter(
-                    c_range[floor(it)], c_range[ceil(it)], it % 1))
-                img[i, j, :] = c_map[idx, :]
+                img[y, x] = sin_col(it, v_freq, v_phase)
 
     return img
 
 
-@ numba.jit
 def lin_inter(x1, x2, t):
     return x1 * (1 - t) + x2 * t
+
+
+def sin_col(iter, v_freq, v_phase):
+    return [sin(f * iter + p) * 127.5 + 127.5 for f, p in zip(v_freq, v_phase)]
 
 
 class Mandelbrot():
@@ -95,28 +126,71 @@ class Mandelbrot():
         self.im_max = Im_center + self.im_height / 2
         self.max_iter = max_iter
 
-    def compute_set(self):
-        self.iterations = mandel_set(
-            self.re_min, self.re_max, self.im_min, self.im_max,
-            self.width, self.height, self.max_iter)
+    def compute_set(self, seed):
+        self._init_colorval(seed)
 
-        return self.iterations
+        xx = np.linspace(self.re_min, self.re_max, num=self.width)
+        yy = np.linspace(self.im_min, self.im_max, num=self.height) * 1j
+        q = np.ravel(xx + yy[:, np.newaxis]).astype(np.complex128)
+        print(q.shape)
 
-    def draw(self):
-        self.im = Image.fromarray(
-            colorize(self.iterations, self.max_iter, self.cmap), 'RGB')
+        start_main = time.time()
+        output = comp_mandelset_opencl(
+            q, self.max_iter, self.amp, self.ofst, self.per)
+        end_main = time.time()
+
+        secs = end_main - start_main
+        print("Main took", secs)
+
+        self.im = Image.fromarray(output.reshape(
+            (self.height, self.width)), 'RGBA')
 
         return self.im
 
+    def draw(self):
+        self.im = Image.fromarray(
+            colorize(self.iterations, self.max_iter, [0.008, 0.006, 0.004], [4, 2, 1]), 'RGB')
+
+        return self.im
+
+    def _init_colorval(self, seed=0):
+        if seed == 0:
+            random.seed()
+            self._seed = random.randint(1, 1000000)
+            random.seed(self._seed)
+
+        else:
+            self._seed = seed
+            random.seed(seed)
+
+        # Gen 3 period
+        self.per = np.asarray([random.uniform(0.01, 0.1)
+                              for i in range(3)], dtype=np.float64)
+
+        # Gen amplitude
+        self.amp = np.asarray([random.uniform(0.0, 127.5)
+                              for i in range(3)], dtype=np.float64)
+
+        # gen offset
+        self.ofst = np.asarray([random.uniform(i, 255.0-i)
+                               for i in self.amp], dtype=np.float64)
+
     def create_colormap(self, seed=-1):
         controlpoints = np.zeros((3, 4))
+        controlpos = [0.0, .6, .85, 1.0]
         if seed == -1:
             # default colormap
             self.seed = -1
 
-            controlpoints[:, :] = [[67, 250, 0, 67],
-                                   [35, 174, 0, 35],
-                                   [113, 123, 0, 113]]
+            # controlpoints[:, :] = [[67, 250, 0, 67],
+            #                        [35, 174, 0, 35],
+            #                        [113, 123, 0, 113]]
+
+            controlpoints = [[0, 0, 10, 148, 233, 238, 202, 187, 174, 155],
+                             [18, 95, 147, 210, 216, 155, 103, 62, 32, 34],
+                             [25, 115, 150, 189, 166, 0, 2, 3, 18, 38]]
+            controlpos = [0, 0.1, 0.2, 0.3, 0.4, 0.6, 0.7, 0.8, 0.9, 1.0]
+
         else:
             if seed == 0:
                 random.seed()
@@ -128,12 +202,16 @@ class Mandelbrot():
                 self.seed = seed
                 random.seed(seed)
 
-            controlpoints[:, 0] = [random.randint(0, 255) for i in range(3)]
-            controlpoints[:, -1] = controlpoints[:, 0]
+            colornum = random.randint(3, 10)
+            controlpoints = np.zeros((colornum, 3))
+            controlpos = np.linspace(0.0, 1.0, colornum)
 
-            controlpoints[:, 1] = [random.randint(0, 255) for i in range(3)]
+            for color in controlpoints:
+                color[:] = [random.randint(0, 255) for i in range(3)]
 
-        self.cmap = colormap(controlpoints)
+            controlpoints = np.transpose(controlpoints)
+
+        self.cmap = colormap(controlpoints, controlpos)
         return self.seed
 
     def save(self, path):
